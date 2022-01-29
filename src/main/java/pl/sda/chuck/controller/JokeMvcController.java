4ee4b539fb9a4196a438117a7cdb08ea59a6d1eb
pecia <peciakm1@gmail.com>
2022-01-29 16:01:27 +0100

    HTML + simple model + list + form

--- src/main/java/pl/sda/chuck/controller/JokeMvcController.java
+++ src/main/java/pl/sda/chuck/controller/JokeMvcController.java
@@ -1,18 +1,26 @@
 package pl.sda.chuck.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import pl.sda.chuck.dao.BaseResponse;
+import pl.sda.chuck.dao.RESPONSE_STATUS;
 import pl.sda.chuck.dto.Joke;
 import pl.sda.chuck.service.JokeService;
 
-import java.util.ArrayList;
+import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.util.stream.Collectors.toList;
+
 @Controller
+@Slf4j
 public class JokeMvcController {
 
     @Autowired
@@ -37,10 +45,23 @@ public class JokeMvcController {
         //IntStream
         List<Joke> jokes = IntStream.range(0, 10)
                 .boxed()
-                .map(integer -> jokeService.getRandomJoke().orElseThrow()).collect(Collectors.toList());
+                .map(integer -> jokeService.getRandomJoke().orElseThrow())
+                .collect(toList());
 
 
         model.addAttribute("jokes", jokes);
         return "joke-list"; // html name, should match
+    }
+
+    @PostMapping("/joke/view/add")
+    public BaseResponse saveJoke(@Valid @RequestBody Joke joke, final BindingResult bindingResult) {
+        log.info("Joke to be saved: {}", joke);
+        jokeService.save(joke);
+        return new BaseResponse(RESPONSE_STATUS.SUCCESS);
+    }
+
+    @GetMapping("/view/form")
+    public String formJoke(Joke joke){
+        return "joke-form";
     }
 }
