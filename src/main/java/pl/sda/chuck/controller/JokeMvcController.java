6002f79e2cd3df5833b3d6b025cb1c696b973129
pecia <peciakm1@gmail.com>
2022-01-29 14:40:33 +0100

    HTML + simple model

--- src/main/java/pl/sda/chuck/controller/JokeMvcController.java
+++ src/main/java/pl/sda/chuck/controller/JokeMvcController.java
@@ -1,15 +1,22 @@
 package pl.sda.chuck.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import pl.sda.chuck.dto.Joke;
+import pl.sda.chuck.service.JokeService;
 
 @Controller
 public class JokeMvcController {
 
+    @Autowired
+    JokeService jokeService;
+
     @GetMapping("/view/joke")
-    public String showJoke() {
-        return "joke";
+    public String showJoke(Model model) { // UI Model which is used to process data between layers (view & controller)
+        Joke joke = jokeService.getRandomJoke().orElseThrow();
+        model.addAttribute("joke", joke); // object joke put in map on key joke
+        return "joke"; // html name, should match
     }
 }
