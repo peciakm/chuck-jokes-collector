16918e9202ea8323c4cb2af8e21154fae0a1a544
pecia <peciakm1@gmail.com>
2022-02-05 13:20:17 +0100

    h2 structur

--- src/main/java/pl/sda/chuck/mapper/JokeMapper.java
+++ src/main/java/pl/sda/chuck/mapper/JokeMapper.java
@@ -2,6 +2,7 @@ package pl.sda.chuck.mapper;
 
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import pl.sda.chuck.dao.JokeEntity;
 import pl.sda.chuck.dto.Joke;
@@ -10,5 +11,12 @@ import pl.sda.chuck.dto.Joke;
 public interface JokeMapper {
     JokeMapper INSTANCE = Mappers.getMapper(JokeMapper.class);
 
+    @Mapping(source = "joke", target = "value.joke")
+    @Mapping(source = "id", target = "value.id")
     Joke jokeEntityToJoke(JokeEntity jokeEntity);
+
+
+@Mapping(source ="value.joke",target ="joke")
+@Mapping(source = "value.id", target ="id")
+JokeEntity jokeToJokeEntity(Joke joke);
 }
